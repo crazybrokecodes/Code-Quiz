2214541cf70a606991598e15faa712f553cee80f
Hyunbin Kang (강현빈) <91191349+crazybrokecodes@users.noreply.github.com>
2021-10-16 18:21:10 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+var startBtn = document.getElementById('startBtn');
+var nextBtn = document.getElementById('nextBtn');
+var quizTitle = document.getElementById('quizTitle');
+var questionDiv = document.getElementById('questions');
+var question = document.getElementById('question');
+var time = document.getElementById('timer');
+
+
+
+
+// Start The Quiz here.
+
+
+startBtn.addEventListener('click', start);
+
+function start() {
+    startBtn.classList.add('hide');
+    quizTitle.classList.add('hide');
+    questionDiv.classList.remove('hide');
+    nextBtn.classList.remove('hide');
+    timer();
+
+
+
+}
+
+
+function displayQuestion() {
+
+
+    var questions = [
+        "Which data type is the following value: false",
+        "Question number 2",
+        "Question number 3"
+    ]
+
+    console.log(questions)
+}
+
+
+
+// Next Button, transit towards - Question 2
+
+nextBtn.addEventListener('click', next1);
+
+function next1() {
+    question1.classList.add('hide');
+    question2.classList.remove('hide');
+}
+
+
+
+// Timer Script
+
+
+
+function timer() {
+    var timeRemaining = 60;
+
+    var timeInterval = setInterval(function() {
+        if (timeRemaining > 1) {
+            time.textContent = timeRemaining;
+            timeRemaining--;
+        } else if (timeRemaining === 1) {
+            timeRemaining--;
+        } else {
+            time.textContent = '';
+            clearInterval(timeInterval);
+        }
+
+    }, 1000);
+}
